c9885fe913faa6715be52e2439ab932c2d318426
Harika Duyu <harika.duyu@flixbus.com>
2022-02-15 19:01:02 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { AllCardsPage } from "./pages/AllCardsPage";
+import { CardPage } from "./pages/CardPage";
+import { CardsPage } from "./pages/CardsPage";
+import { HomePage } from "./pages/HomePage";
+import { NewCardForm } from "./pages/NewCardForm";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="cards" >
+          <Route path=":cardId" element={<CardPage />} />
+          <Route path="new" element={<NewCardForm />} />
+          <Route index element={<CardsPage /> }/>
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
-export default App;
+
+
